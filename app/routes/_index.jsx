519fb0412d24b2375d74494d5ea2c86056d9c212
hedlundemma <emmahedlund93@gmail.com>
2023-10-17 13:18:34 +0200

    first route index

---
+++ app/routes/_index.jsx
@@ -0,0 +1,14 @@
+export function meta() {
+    return [
+      {title: 'Maouflowers'},
+      {description: 'A graduation work creating a shopify store with Hydrogen'},
+    ];
+  }
+
+  export default function Index() {
+    return (
+      <div>
+        <h1>Hello from the home page!</h1>
+      </div>
+    );
+  }
