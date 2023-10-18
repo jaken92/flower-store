6ea88a538b4a92e2f50a2c9a37f054d125ef53ac
jaken92 <jaaakeeen@gmail.com>
2023-10-18 13:48:45 +0200

    product handler + product options component

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -1,68 +1,83 @@
+import ProductOptions from '~/components/ProductOptions';
 import {json} from '@shopify/remix-oxygen';
 import {useLoaderData} from '@remix-run/react';
 import {Image} from '@shopify/hydrogen-react';
 
 const seo = ({data}) => ({
-    title: data?.product?.title,
-    description: data?.product?.description.substr(0, 154),
-  });
-  export const handle = {
-    seo,
-  };
+  title: data?.product?.title,
+  description: data?.product?.description.substr(0, 154),
+});
+export const handle = {
+  seo,
+};
 
-export async function loader({params, context}) {
-    const {handle} = params; // Destructure the handle from the URL params
-    const {product} = await context.storefront.query(PRODUCT_QUERY, {
-      variables: {
-        handle, // Pass the handle to the GraphQL query
-      },
-    });
-  
-    if (!product?.id) {
-      throw new Response(null, {status: 404});
-    }
-  
-    return json({
+export async function loader({params, context, request}) {
+  const {handle} = params;
+  const searchParams = new URL(request.url).searchParams;
+  const selectedOptions = [];
+
+  // set selected options from the query string
+  searchParams.forEach((value, name) => {
+    selectedOptions.push({name, value});
+  });
+
+  const {product} = await context.storefront.query(PRODUCT_QUERY, {
+    variables: {
       handle,
-      product,
-    });
-  };
+      selectedOptions,
+    },
+  });
+
+  if (!product?.id) {
+    throw new Response(null, {status: 404});
+  }
+
+  return json({
+    product,
+  });
+}
 
 export default function ProductHandle() {
-    const {handle, product} = useLoaderData();
-    console.log(product);
-    return (
-        <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
-          <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
-            <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-hidden md:grid-cols-2 md:w-full lg:col-span-2">
-              <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
-              <Image
-  className={`w-full h-full aspect-square object-cover`}
-  data={product.featuredImage}
-/>
-              </div>
-            </div>
-            <div className="md:sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-2 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]">
-              <div className="grid gap-2">
-                <h1 className="text-4xl font-bold leading-10 whitespace-normal">
-                  {product.title}
-                </h1>
-                <span className="max-w-prose whitespace-pre-wrap inherit text-copy opacity-50 font-medium">
-                  {product.vendor}
-                </span>
-              </div>
-              <h3>TODO Product Options</h3>
-              <div
-                className="prose border-t border-gray-200 pt-6 text-black text-md"
-                dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
-              ></div>
-            </div>
+  const {handle, product} = useLoaderData();
+  console.log(product);
+  return (
+    <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
+      <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-hidden md:grid-cols-2 md:w-full lg:col-span-2">
+          <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
+            <Image
+              className={`w-full h-full aspect-square object-cover`}
+              data={product.selectedVariant?.image || product.featuredImage}
+            />
           </div>
-        </section>
-      );
-    }
-  const PRODUCT_QUERY = `#graphql
-  query product($handle: String!) {
+        </div>
+        <div className="md:sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-2 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]">
+          <div className="grid gap-2">
+            <h1 className="text-4xl font-bold leading-10 whitespace-normal">
+              {product.title}
+            </h1>
+            <span className="max-w-prose whitespace-pre-wrap inherit text-copy opacity-50 font-medium">
+              {product.vendor}
+            </span>
+          </div>
+          <ProductOptions options={product.options} />
+          {/* Delete this after verifying */}
+          <p>Selected Variant: {product.selectedVariant?.id}</p>
+          <div
+            className="prose border-t border-gray-200 pt-6 text-black text-md"
+            dangerouslySetInnerHTML={{__html: product.descriptionHtml}}
+          />
+          <div
+            className="prose border-t border-gray-200 pt-6 text-black text-md"
+            dangerouslySetInnerHTML={{__html: product.descriptionHtml}}
+          ></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+const PRODUCT_QUERY = `#graphql
+  query product($handle: String!, $selectedOptions: [SelectedOptionInput!]!) {
     product(handle: $handle) {
       id
       title
@@ -80,6 +95,58 @@ export default function ProductHandle() {
       options {
         name,
         values
+      }
+      selectedVariant: variantBySelectedOptions(selectedOptions: $selectedOptions) {
+        id
+        availableForSale
+        selectedOptions {
+          name
+          value
+        }
+        image {
+          id
+          url
+          altText
+          width
+          height
+        }
+        price {
+          amount
+          currencyCode
+        }
+        compareAtPrice {
+          amount
+          currencyCode
+        }
+        sku
+        title
+        unitPrice {
+          amount
+          currencyCode
+        }
+        product {
+          title
+          handle
+        }
+      }
+      variants(first: 1) {
+        nodes {
+          id
+          title
+          availableForSale
+          price {
+            currencyCode
+            amount
+          }
+          compareAtPrice {
+            currencyCode
+            amount
+          }
+          selectedOptions {
+            name
+            value
+          }
+        }
       }
     }
   }
