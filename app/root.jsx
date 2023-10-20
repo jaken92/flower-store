0ce537f19824735a6b14a96068c240b1c4ae2fed
jaken92 <jaaakeeen@gmail.com>
2023-10-20 13:19:08 +0200

    added flowerHero in public. Removed import of reset.css in root

--- app/root.jsx
+++ app/root.jsx
@@ -14,7 +14,7 @@ import {
   isRouteErrorResponse,
 } from '@remix-run/react';
 import favicon from '../public/favicon.svg';
-import resetStyles from './styles/reset.css';
+// import resetStyles from './styles/reset.css';
 import appStyles from './styles/app.css';
 import {Layout} from '~/components/Layout';
 import tailwindCss from './styles/tailwind.css';
@@ -37,7 +37,6 @@ export const shouldRevalidate = ({formMethod, currentUrl, nextUrl}) => {
 export function links() {
   return [
     {rel: 'stylesheet', href: tailwindCss},
-    {rel: 'stylesheet', href: resetStyles},
     {rel: 'stylesheet', href: appStyles},
     {
       rel: 'preconnect',
