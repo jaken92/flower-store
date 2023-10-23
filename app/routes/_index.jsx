feeadb048470dd6afb1442eb1a71d8ac87a10125
hedlundemma <emmahedlund93@gmail.com>
2023-10-23 12:22:50 +0200

    routing index to other pages

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,4 +1,5 @@
-import {useLoaderData, Link} from '@remix-run/react';
+import {useLoaderData} from '@remix-run/react';
+import {Link} from 'react-router-dom';
 import {Image} from '@shopify/hydrogen';
 
 export function meta() {
@@ -14,11 +15,18 @@ export function meta() {
 
 export default function Index() {
   return (
-    <section className="w-full gap-4">
-      <img
-        className="w-full h-screen object-cover"
-        src="./images/heroes/flowerHero.jpg"
-      ></img>
-    </section>
+    <>
+      <section className="w-full gap-4">
+        <img
+          className="w-full h-screen object-cover"
+          src="./images/heroes/flowerHero.jpg"
+        ></img>
+      </section>
+      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
+        <Link to={`/collections`}>Order flowers</Link>
+        <Link to={`/pages/weddings`}>Weddings</Link>
+        <Link to={`/pages/subscriptions`}>Subscriptions</Link>
+      </div>
+    </>
   );
 }
