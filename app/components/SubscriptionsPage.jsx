79bb2b98ed2188cab80ce009b66e5c1e26fc2e7a
jaken92 <jaaakeeen@gmail.com>
2023-10-24 12:07:59 +0200

    Create SubscriptionsPage.jsx

---
+++ app/components/SubscriptionsPage.jsx
@@ -0,0 +1,9 @@
+export default function SubscriptionsPage({content, title}) {
+  console.log(content);
+  console.log(title);
+  return (
+    <>
+      <p>Hello from SubscriptionsPage-component</p>
+    </>
+  );
+}
