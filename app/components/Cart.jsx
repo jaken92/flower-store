ed5450736b9d7d9dd136d17b47ca7e1d244b3745
jaken92 <jaaakeeen@gmail.com>
2023-11-02 14:09:07 +0100

    styling cart

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -335,11 +335,14 @@ function NoteForm() {
 
   return (
     <>
-      <p>
+      <p className="m-3 font-custom">
         Want to write a message for the giftcard or have any special requests
         for your order?
       </p>
-      <button onClick={getNoteValues} className="hover:cursor-pointer">
+      <button
+        onClick={getNoteValues}
+        className="hover:cursor-pointer m-3 rounded-2xl font-custom bg-teal border-2 border-teal p-3"
+      >
         Add message
       </button>
       <div
