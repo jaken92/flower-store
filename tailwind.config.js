7e0983bfb8e0beccf3d9d792c3ab796d12a9cd51
jaken92 <jaaakeeen@gmail.com>
2023-11-02 14:19:08 +0100

    removed screens from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,22 +17,6 @@ export default {
         customSemiBold: ['Playfair Display SemiBold'],
         customMedium: ['Playfair Display Medium'],
       },
-      screens: {
-        sm: '640px',
-        // => @media (min-width: 640px) { ... }
-
-        md: '768px',
-        // => @media (min-width: 768px) { ... }
-
-        lg: '1024px',
-        // => @media (min-width: 1024px) { ... }
-
-        xl: '1280px',
-        // => @media (min-width: 1280px) { ... }
-
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
-      },
     },
   },
 };
