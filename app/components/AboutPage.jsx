86afdcb87eb5934bb364f24346e58f38092fe64f
jaken92 <jaaakeeen@gmail.com>
2023-11-22 17:41:27 +0100

    Updated link to img and alt text in aboutPage

--- app/components/AboutPage.jsx
+++ app/components/AboutPage.jsx
@@ -77,8 +77,8 @@ export default function AboutPage({
         <img
           loading="lazy"
           className="object-cover md:w-[50%] mt-4"
-          src="../images/aboutImg2Final.jpg"
-          alt="founder of MouaFlowers arranging pink and yellow flowers"
+          src="../images/AboutImg2Final.jpg"
+          alt="founder of MouaFlowers holding a bouquet"
         />
       </div>
       <div
